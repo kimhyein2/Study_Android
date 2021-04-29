4f8792e6f4a471b60b9a63ffe162c21c391efe89
kimhyein <kt01030426474@gmail.com>
2021-04-29 17:51:35 +0900

    43 finish

--- My43_AsyncTask/app/src/main/java/com/example/my43_asynctask/MainActivity.java
+++ My43_AsyncTask/app/src/main/java/com/example/my43_asynctask/MainActivity.java
@@ -9,24 +9,36 @@ import android.widget.ProgressBar;
 public class MainActivity extends AppCompatActivity {
 
     ProgressBar progressBar;
+    BackgroundTask backgroundTask;
+    int value = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        progressBar = findViewById(R.id.progressBar);
+
+
         //실행
         findViewById(R.id.btnExe).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //AsyncTask 객체 만들고 실행하기
+                backgroundTask = new BackgroundTask(progressBar, value);
+                backgroundTask.execute();
 
             }
         });
 
+        //취소
         findViewById(R.id.btnCancel).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(backgroundTask != null){
+                    backgroundTask.cancel(true);
 
+                }
             }
         });
     }
